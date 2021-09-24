373f3603731fff2724cde98013b42061e0f67698
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-09-24 11:01:38 +0330

    functionality added to buttons
    still have some buges when its used with idm and similar appes

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-
 const sounds = document.querySelectorAll('audio');
-
+const stopSongs = () => {
+  sounds.forEach((sound) => {
+    sound.pause;
+    sound.currentTime = 0;
+  });
+};
 sounds.forEach((sound) => {
   const buttonEl = document.createElement('button');
   buttonEl.classList.add('btn');
-
+  buttonEl.addEventListener('click', () => {
+    stopSongs();
+    sound.play();
+  });
   buttonEl.innerText = sound.id;
 
   document.getElementById('buttons').append(buttonEl);
