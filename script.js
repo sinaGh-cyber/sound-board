8b213ff7f55d89c5fb7f4b76459da66eb343c10e
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-09-24 10:13:10 +0330

    button generator coded in JS

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+const sounds = document.querySelectorAll('audio');
+
+sounds.forEach((sound) => {
+  const buttonEl = document.createElement('button');
+  buttonEl.classList.add('btn');
+
+  buttonEl.innerText = sound.id;
+
+  document.getElementById('buttons').append(buttonEl);
+});
